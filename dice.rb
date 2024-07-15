51638e4af7aa869de0b95c69a8994276b3a6b041
nhatminhdao <nhatminhdao@gmail.com>
2024-07-15 00:41:13 +0000

    wrapper

--- dice.rb
+++ dice.rb
@@ -29,7 +29,7 @@ get ("/dice/2d6") do
   second_die = rand (1..6)
   sum = first_die + second_die
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  erb(:two_six)
+  erb(:two_six, { :layout => :wrapper })
 end
 
 get ("/dice/2d10") do
@@ -37,13 +37,13 @@ get ("/dice/2d10") do
   second_die = rand(1..10)
   sum = first_die + second_die
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-  erb(:two_ten)
+  erb(:two_ten, { :layout => :wrapper })
 end
 
 get ("/dice/1d20") do
   die = rand (1..20)
   @outcome = "You rolled a #{die}."
-  erb(:one_twenty)
+  erb(:one_twenty, { :layout => :wrapper })
 end
 
 get ("/dice/5d4") do
@@ -54,5 +54,5 @@ get ("/dice/5d4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die} and a #{fifth_die} for a total of #{sum}."
-  erb(:five_four)
+  erb(:five_four, { :layout => :wrapper })
 end
